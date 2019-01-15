daddaee47dc90c7bcffe9fbce3d87412825ad761
Ajil Oommen <ajilo297@gmail.com>
2019-01-15 20:15:44 +0530

    Initial Commit

---
+++ MyParentsOnBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyParentsOnBoard
+//
+//  Created by ATS on 15/01/19.
+//  Copyright © 2019 ATS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
