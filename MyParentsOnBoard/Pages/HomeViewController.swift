51ecadce6421d30afc25e94fe2dfccef38ec73e9
Ajil Oommen <ajilo297@gmail.com>
2019-01-20 08:58:42 +0530

    Added Home View Controller

---
+++ MyParentsOnBoard/Pages/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  MyParentsOnBoard
+//
+//  Created by Ajil on 20/01/19.
+//  Copyright © 2019 ATS. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
